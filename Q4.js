2548d68241dd12f057fa241e279b1c837f0e083c
Necromancer-D <davisgamesplay@gmail.com>
2026-04-18 19:02:14 +0100

    Add fruit manipulation example in Q4.js

---
+++ Q4.js
@@ -0,0 +1,11 @@
+const fruits = ["mango", "orange", "banana"];
+
+// Lets Add pineapple to the end
+fruits.push("pineapple");
+
+// Lets Remove the first item
+fruits.shift();
+
+// Log results
+console.log(fruits);
+console.log(fruits.length);
